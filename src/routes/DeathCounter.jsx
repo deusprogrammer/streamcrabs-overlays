bb6ef92553c7295ec5e87ba1f05910caff92063d
Michael C. Main <deusprogrammer@gmail.com>
2021-04-06 23:14:19 -0500

    Added websocket updates

--- src/routes/DeathCounter.jsx
+++ src/routes/DeathCounter.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+let urlParams = new URLSearchParams(window.location.search);
 
 const fontSize = 32;
 
@@ -9,7 +12,45 @@ class DeathCounter extends React.Component {
 		direction: 1
 	}
 
+	connect = async () => {
+        const ws = new W3CWebSocket('wss://deusprogrammer.com/api/ws/twitch');
+
+        ws.onopen = () => {
+            ws.send(JSON.stringify({
+                type: "REGISTER_PANEL",
+                from: "PANEL",
+                channelId: urlParams.get("channelId")
+            }));
+        };
+
+        ws.onmessage = async (message) => {
+            let event = JSON.parse(message.data);
+            
+			if (event.type === "DEATH_COUNT") {
+				this.onDeath();
+			}
+        };
+
+        ws.onclose = async (e) => {
+            console.log('Socket is closed. Reconnect will be attempted in 5 second.', e.reason);
+            this.setState({ mobs: [] });
+            setTimeout(async () => {
+                this.connect();
+            }, 5000);
+        };
+
+        ws.onerror = async (err) => {
+            console.error('Socket encountered error: ', err.message, 'Closing socket');
+            ws.close();
+        };
+    }
+
 	componentDidMount() {
+		// If a channel id is supplied, connect the websocket for updates via bot commands
+		if (urlParams.get("channelId")) {
+			this.connect();
+		}
+		
 		document.addEventListener("click", this.onDeath);
         document.addEventListener("contextmenu", this.onReset);
 	}
@@ -25,7 +66,7 @@ class DeathCounter extends React.Component {
 		return false;
 	}
 
-	onDeath = (e) => {
+	onDeath = () => {
 		this.setState((prevState) => {
 			return {
 				deaths: prevState.deaths + 1,
