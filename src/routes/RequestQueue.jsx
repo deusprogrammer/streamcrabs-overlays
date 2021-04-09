73e44127f2ce2288831256068ae21b2973acd981
Michael C. Main <deusprogrammer@gmail.com>
2021-04-09 15:29:48 -0500

    Made bottom part a slider

--- src/routes/RequestQueue.jsx
+++ src/routes/RequestQueue.jsx
@@ -58,36 +58,37 @@ class RequestQueue extends React.Component {
 
         // Cycle between next up view and queue view
         setInterval(() => {
-            this.setState({mode: "QUEUE"});
-            setTimeout(() => {
-                this.setState({mode: "NEXT_UP"})
-            }, 1000 * 10);
+            this.showQueue();
         }, 1000 * 60);
 	}
+
+    showQueue = () => {
+        this.setState({mode: "QUEUE"});
+        setTimeout(() => {
+            this.setState({mode: "NEXT_UP"})
+        }, 1000 * 10);
+    }
 
 	render() {
 		return (
 			<div style={{height: "100vh", width: "100vw", userSelect: "none", position: "relative"}} className="App">
-				{this.state.mode === "NEXT_UP" ?
-                    <div style={{width: "100vw", position: "absolute", bottom: "0px", left: "0px", textAlign: "center"}}>
+                    <div style={{width: "100vw", position: "absolute", bottom: "0px", left: "0px", textAlign: "center", fontSize: "20pt", WebkitTextStroke: "1px black", WebkitTextFillColor: "white"}} onClick={this.showQueue}>
                         {this.state.requestList.length > 0 ? <marquee>Next up: {this.state.requestList[this.state.requestList.length - 1].request} from {this.state.requestList[0].requester}</marquee>
                         :
                         <marquee>No requests so far.  Make a request by donating!</marquee>}
-                    </div> 
-                    :
+                    </div>
                     <div style={{width: "100vw", position: "absolute", bottom: "0px", left: "0px", textAlign: "center"}}>
-                        <h3>Coming up:</h3>
-                        <table>
-                            {this.state.requestList.reverse().map((request, index) => {
+                        <div className={this.state.mode === "QUEUE" ? "open" : "closed"} style={{maxWidth: "50%", margin: "auto", padding: "0px 5px", backgroundColor: "gray", color: "white"}}>
+                            <strong>Coming up:</strong>
+                            {this.state.requestList.reverse().slice(0, 3).map((request, index) => {
                                 return (
-                                    <div key={`entry-${index}`} style={{color: index === 0 ? "red" : "black"}}>
+                                    <div key={`entry-${index}`} style={{color: index === 0 ? "yellow" : "white"}}>
                                         {index + 1}: {request.request} from {request.requester}
                                     </div>
                                 )
                             })}
-                        </table>
-                    </div> 
-                }
+                        </div>
+                    </div>
 			</div>
 		);
 	}
