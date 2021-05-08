dec21a8cc0711d253177a3b517cb6dffd26f26f6
Michael C. Main <deusprogrammer@gmail.com>
2021-05-08 03:32:22 -0500

    Simplified overlay functions

--- src/App.js
+++ src/App.js
@@ -5,17 +5,15 @@ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import DeathCounter from './routes/DeathCounter';
 import SoundPlayer from './routes/SoundPlayer';
 import Home from './routes/Home';
-import CameraObscura from './routes/CameraObscura';
 import RequestQueue from './routes/RequestQueue';
-import BadApple from './routes/BadApple';
+import TwitchMultiOverlay from './routes/multi/TwitchMultiOverlay';
 
 class App extends React.Component {
 	render() {
 		return (
 			<Router>
 				<Switch>
-					<Route path={`${process.env.PUBLIC_URL}/badapple`} component={BadApple} exact />
-					<Route path={`${process.env.PUBLIC_URL}/birdup`} component={CameraObscura} exact />
+					<Route path={`${process.env.PUBLIC_URL}/multi`} component={TwitchMultiOverlay} exact />
 					<Route path={`${process.env.PUBLIC_URL}/death-counter`} component={DeathCounter} exact />
 					<Route path={`${process.env.PUBLIC_URL}/sound-player`} component={SoundPlayer} exact />
 					<Route path={`${process.env.PUBLIC_URL}/requests`} component={RequestQueue} exact />
---
+++ src/routes/multi/TwitchMultiOverlay.jsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import BadApple from './BadApple';
+import BirdUp from './BirdUp';
+import RandomVideo from './RandomVideo';
+import { w3cwebsocket as W3CWebSocket } from "websocket";
+
+let urlParams = new URLSearchParams(window.location.search);
+
+export default class TwitchMultiOverlay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.playing = false;
+        this.queue = [];
+
+        this.state = {
+            show: {
+                birdup: false,
+                badapple: false,
+                randomvideo: false
+            }
+        }
+    }
+
+    connect = async () => {
+        const ws = new W3CWebSocket('wss://deusprogrammer.com/api/ws/twitch');
+
+        ws.onopen = () => {
+            ws.send(JSON.stringify({
+                type: "REGISTER_PANEL",
+                from: "PANEL",
+                channelId: urlParams.get("channelId")
+            }));
+
+            setInterval(() => {
+                ws.send(JSON.stringify({
+                    type: "PING_SERVER",
+                    from: "PANEL",
+                    channelId: urlParams.get("channelId")
+                }));
+            }, 20 * 1000);
+        };
+
+        ws.onmessage = async (message) => {
+            let event = JSON.parse(message.data);
+
+            console.log("EVENT: " + event.type);
+            
+			switch(event.type) {
+                case "BADAPPLE":
+                    this.queue.push("badapple");
+                    break;
+                case "RANDOM_VIDEO":
+                    this.queue.push("randomvideo");
+                    break;
+                case "BIRDUP":
+                    this.queue.push("birdup");
+                    break;
+			}
+        };
+
+        ws.onclose = async (e) => {
+            console.log('Socket is closed. Reconnect will be attempted in 5 second.', e.reason);
+            this.setState({ mobs: [] });
+            setTimeout(async () => {
+                this.connect();
+            }, 5000);
+        };
+
+        ws.onerror = async (err) => {
+            console.error('Socket encountered error: ', err.message, 'Closing socket');
+            ws.close();
+        };
+    }
+
+    consumer = async () => {
+        if (this.queue.length <= 0 || this.playing) {
+            return;
+        }
+
+        let entry = this.queue[0];
+        this.queue = this.queue.slice(1);
+        this.playing = true;
+
+        this.enable(entry);
+    }
+
+    componentDidMount() {
+        // If a channel id is supplied, connect the websocket for updates via bot commands
+		if (urlParams.get("channelId")) {
+			this.connect();
+		}
+
+        setInterval(this.consumer, 1000);
+
+        if ('requestVideoFrameCallback' in HTMLVideoElement.prototype) {
+            console.log("Video API is supported");
+        }
+    }
+
+    enable = (key) => {
+        let show = {...this.state.show};
+        show[key] = true;
+        this.setState({show});
+        this.playing = true;
+    }
+
+    reset = (key) => {
+        let show = {...this.state.show};
+        show[key] = false;
+        this.setState({show});
+        this.playing = false;
+    }
+
+    render() {
+        let showComponent = null;
+        if (this.state.show.birdup) {
+            showComponent = <BirdUp onComplete={this.reset} />;
+        } else if (this.state.show.badapple) {
+            showComponent = <BadApple onComplete={this.reset} />;
+        } else if (this.state.show.randomvideo) {
+            showComponent = <RandomVideo onComplete={this.reset} />;
+        }
+
+        return (
+            <div>
+                <div className="multContainer">
+                    {showComponent}
+                </div>
+            </div>
+        )
+    }
+}
