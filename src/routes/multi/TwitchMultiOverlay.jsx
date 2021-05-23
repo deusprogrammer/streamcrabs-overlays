4557c094ca98644b6d7fc82577c403e99b461d48
Michael C. Main <deusprogrammer@gmail.com>
2021-05-23 00:03:23 -0500

    Improved video player

--- src/routes/multi/TwitchMultiOverlay.jsx
+++ src/routes/multi/TwitchMultiOverlay.jsx
@@ -10,18 +10,10 @@ let urlParams = new URLSearchParams(window.location.search);
 export default class TwitchMultiOverlay extends React.Component {
     constructor(props) {
         super(props);
-        this.playing = false;
         this.queue = [];
-        this.event = {};
 
         this.state = {
-            show: {
-                birdup: false,
-                badapple: false,
-                // randomvideo: false,
-                randomcustomvideo: false,
-                wtd: false
-            }
+            currentEvent: null
         }
     }
 
@@ -47,27 +39,13 @@ export default class TwitchMultiOverlay extends React.Component {
         ws.onmessage = async (message) => {
             let event = JSON.parse(message.data);
 
-            // console.log("EVENT: " + JSON.stringify(event, null, 5));
-            
-			switch(event.type) {
-                case "BADAPPLE":
-                    this.queue.push("badapple");
-                    break;
-                // case "RANDOM_VIDEO":
-                //     this.queue.push("randomvideo");
-                //     break;
-                case "RANDOM_CUSTOM_VIDEO":
-                    this.queue.push("randomcustomvideo");
-                    this.url = event.eventData.url;
-                    break;
-                case "BIRDUP":
-                    this.queue.push("birdup");
-                    break;
-                case "DUB":
-                    this.queue.push("wtd");
-                    this.event = event;
-                    break;
-			}
+            if (!["BIRDUP", "RANDOM_CUSTOM_VIDEO", "BADAPPLE"].includes(event.type)) {
+                return;
+            }
+
+            console.log("Received: " + JSON.stringify(event, null, 5));
+
+            this.queue.push(event);
         };
 
         ws.onclose = async (e) => {
@@ -85,15 +63,17 @@ export default class TwitchMultiOverlay extends React.Component {
     }
 
     consumer = async () => {
-        if (this.queue.length <= 0 || this.playing) {
+        if (this.queue.length <= 0 || this.state.currentEvent) {
             return;
         }
 
-        let entry = this.queue[0];
+        console.log("EVENT");
+
+        let currentEvent = this.queue[0];
         this.queue = this.queue.slice(1);
         this.playing = true;
 
-        this.enable(entry);
+        this.setState({currentEvent});
     }
 
     componentDidMount() {
@@ -103,36 +83,40 @@ export default class TwitchMultiOverlay extends React.Component {
 		}
 
         setInterval(this.consumer, 1000);
-
-        if ('requestVideoFrameCallback' in HTMLVideoElement.prototype) {
-            console.log("Video API is supported");
-        }
     }
 
-    enable = (key) => {
-        let show = {...this.state.show};
-        show[key] = true;
-        this.setState({show});
-        this.playing = true;
-    }
-
-    reset = (key) => {
-        let show = {...this.state.show};
-        show[key] = false;
-        this.setState({show});
-        this.playing = false;
+    reset = () => {
+        this.setState({currentEvent: null});
     }
 
     render() {
         let showComponent = null;
-        if (this.state.show.birdup) {
-            showComponent = <BirdUp onComplete={this.reset} />;
-        } else if (this.state.show.badapple) {
-            showComponent = <BadApple onComplete={this.reset} />;
-        } else if (this.state.show.randomcustomvideo) {
-            showComponent = <RandomCustomVideo onComplete={this.reset} url={this.url} />;
-        } else if (this.state.show.wtd) {
-            showComponent = <WhatTheDub onComplete={this.reset} url={this.event.eventData.videoData.videoUrl} subtitles={this.event.eventData.videoData.subtitles} substitution={this.event.eventData.substitution} />;
+
+        console.log(JSON.stringify(this.state.currentEvent, null, 5));
+
+        if (!this.state.currentEvent) {
+            return (<div></div>)
+        }
+
+        switch(this.state.currentEvent.type) {
+            case "BADAPPLE":
+                showComponent = <BadApple onComplete={this.reset} />;
+                break;
+            case "RANDOM_CUSTOM_VIDEO":
+                showComponent = <RandomCustomVideo onComplete={this.reset} url={this.state.currentEvent.eventData.url} />;
+                break;
+            case "BIRDUP":
+                showComponent = <BirdUp onComplete={this.reset} />;
+                break;
+            // case "DUB":
+                // showComponent = (
+                //     <WhatTheDub 
+                //         onComplete={this.reset} 
+                //         url={this.state.currentEvent.eventData.videoData.videoUrl} 
+                //         subtitles={this.state.currentEvent.eventData.videoData.subtitles} 
+                //         substitution={this.state.currentEvent.eventData.substitution} />
+                // )
+                // break;
         }
 
         return (
